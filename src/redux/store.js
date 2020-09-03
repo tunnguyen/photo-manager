e2ef449ce94dfa819618691baeda2dfcadb2dfb5
Tung Nguyen <tung.nguyen@mirumagency.com>
2020-09-03 15:00:36 +0700

    remove redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,14 +4,9 @@ import rootReducer from './reducers';
 
 const middleWares = [thunk];
 const initialState = {};
-const applyMW = process.env.NODE_ENV !== 'production' ?
-  compose(
-    applyMiddleware(...middleWares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ) : applyMiddleware(...middleWares);
 
 export default createStore(
   rootReducer,
   initialState,
-  applyMW
+  applyMiddleware(...middleWares)
 );
